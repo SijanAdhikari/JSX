69fd7091b8b335cc8025e489294fd3772d4ec32a
Sijan Adhikari <adhikarisijan909@gmail.com>
2026-07-16 19:04:44 +0545

    inline styling

--- 283-jsx-attributes-and-styling/src/index.jsx
+++ 283-jsx-attributes-and-styling/src/index.jsx
@@ -2,9 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 const img = "https://picsum.photos/200"
+const customStyle ={        //Inline styling
+  color:"red",
+  fontsize:"20px",
+  border:"1px solid black"
+}
+
+customStyle.color="blue";
 
 ReactDOM.render(
   <div>
+    <h1 style={customStyle}>Hello World!</h1> 
     <h1 className = "heading" contentEditable="true" spellCheck="false">My Favourite Foods</h1>
     <div>
       <img alt="random image" src={img}></img>
@@ -17,8 +25,4 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you're running this locally in VS Code use the commands:
-// npm install
-// to install the node modules and
-// npm run dev
-// to launch your react project in your browser
+
