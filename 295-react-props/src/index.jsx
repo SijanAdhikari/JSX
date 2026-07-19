3628ce29d65a42b93483a88d963c42bf910a1f73
Sijan Adhikari <adhikarisijan909@gmail.com>
2026-07-20 00:18:07 +0545

    react props

---
+++ 295-react-props/src/index.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Card(props){
+  return(
+  <div>
+    <h2>{props.name}</h2>
+    <img
+      className={props.className}
+      src={props.img}
+      alt="avatar_img"
+    />
+    <p>{props.tel}</p>
+    <p>{props.email}</p>
+    </div>);
+
+}
+
+ReactDOM.render(
+  <div>
+     <h1>My Contacts</h1>
+     <Card name="Beyonce" 
+     img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+     tel="+123 456 789"
+     email="b@beyonce.com"
+     />
+
+     <Card className="leo" name="Leonardo Dicaprio"
+     img="https://cdn.britannica.com/65/227665-050-D74A477E/American-actor-Leonardo-DiCaprio-2016.jpg"
+     tel="+134 657 345"
+     email="leodicaprio.com"
+     />
+  
+  </div>,
+  document.getElementById("root")
+);
+
+
