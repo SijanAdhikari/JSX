79b0bce6680f27536532fc264953272ab907db02
Sijan Adhikari <adhikarisijan909@gmail.com>
2026-07-26 18:37:10 +0545

    conditional rendering

---
+++ 303-conditional-rendering/src/components/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Login from "./Login";
+
+const loggedIn= false;
+
+function App() {
+  return (
+    <div className="container">
+      {loggedIn ? <h1>Hello</h1> : <Login />}
+    </div>
+  );
+}
+
+export default App;
+``
