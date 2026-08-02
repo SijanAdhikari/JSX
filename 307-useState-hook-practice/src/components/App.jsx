b2f2497a6f9d3691b24676d4839d4967bda5a424
Sijan Adhikari <adhikarisijan909@gmail.com>
2026-08-02 23:15:35 +0545

    useState integration on timer

--- 307-useState-hook-practice/src/components/App.jsx
+++ 307-useState-hook-practice/src/components/App.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import {useState} from "react";
 
 function App() {
+  setInterval(currTime, 1000);
+  
+  const [currentTime, setTime] = useState("");
+
+
+  function currTime(){
+    let time = new Date().toLocaleTimeString();
+    console.log(time);
+    setTime(time);
+  }
   return (
     <div className="container">
-      <h1>TIME</h1>
-      <button>Get Time</button>
+      <h1>{currentTime}</h1>
+      <button onClick={currTime} >Get Time</button>
     </div>
   );
 }
