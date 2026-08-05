798c18eca3bdbf112b673ee12eb3d77f3c800a38
Sijan Adhikari <adhikarisijan909@gmail.com>
2026-08-05 23:05:22 +0545

    class components

--- 311-react-forms/src/components/App.jsx
+++ 311-react-forms/src/components/App.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import {useState} from "react";
 
 function App() {
+const [buttonColor, setButtonColor] = useState("white");
+const [name, setName] =useState("");
+const [headingText, setHeadingText] = useState("Hello");
   return (
     <div className="container">
-      <h1>Hello </h1>
-      <input type="text" placeholder="What's your name?" />
-      <button>Submit</button>
+      <h1>{headingText}</h1>
+      <input type="text" placeholder="What's your name?" value={name} onChange={(event)=>setName(event.target.value)}/>
+      <button type='submit' style={{backgroundColor:buttonColor}} onMouseOver={()=>setButtonColor("black")} onMouseOut={()=>setButtonColor("white")} onClick={()=>{
+            setHeadingText(name)
+        }}>Submit</button>
     </div>
   );
 }
